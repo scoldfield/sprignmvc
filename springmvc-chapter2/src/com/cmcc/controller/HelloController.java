bd2a8a3f4afc8579966c4f1bdd6d242149887480
Scoldfield <530314297@qq.com>
2016-07-18 09:47:02 +0800

    springmvc

---
+++ springmvc-chapter2/src/com/cmcc/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.cmcc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller{    //页面控制器/处理器必须实现org.springframework.web.servlet.mvc.Controller接口，注意别选错了，后面我们还会学习其他的处理器实现方式
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        //1、收集参数
+        //2、绑定参数到命令对象
+        //3、调用业务对象
+        //4、选择下一个页面
+        ModelAndView mv = new ModelAndView();
+        //添加模型数据，可以是任意的POJO对象
+        mv.addObject("message", "Hello World!");
+        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+        mv.setViewName("hello");
+        return mv;
+    }
+
+}
